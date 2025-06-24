ec51dc08b7959fd60a56e91be17709f938a397b0
MounikaJangam <121105776+MounikaJangam@users.noreply.github.com>
2025-06-24 16:15:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import About from './components/About';
+// import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
